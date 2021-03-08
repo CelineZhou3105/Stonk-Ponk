abba5b8ce314d26ebe0dce14d5fa6fdb545a14e8
Sampath Somanchi <sampathanator@gmail.com>
2021-03-08 20:48:29 +1100

    Even better

--- stonk-ponk-be/main.py
+++ stonk-ponk-be/main.py
@@ -3,18 +3,19 @@ from pymongo import MongoClient
 import pprint
 
 def main():
-    start_db()
+    db = start_db()
     
 
 def start_db():
     client = MongoClient('localhost', 27017)
     db = client.test_database
 
-    user_auth = {"u_name": "Ash",
-    "pwd": "Sarkar"}
-
     user_auths = db.user_auths
 
     pprint.pprint(user_auths.find_one())
+    return db
+
+def insert_user(username, password):
+
 if __name__ == '__main__':
     main()
