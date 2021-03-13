228b4b73b2a33196ee95034de2fa90175ca31115
Sampath Somanchi <sampathanator@gmail.com>
2021-03-13 18:59:17 +1100

    Database functions moved into their own file. User auth only needs pwd when making account and logging in

---
+++ stonk-ponk-be/database_func.py
@@ -0,0 +1,52 @@
+import pymongo
+from pymongo import MongoClient
+import pprint
+
+#Creates a connection to a mongodb created on localhost and returns db pointer/client
+def start_db():
+    client = MongoClient('localhost', 27017)
+
+    db = client.test_database
+
+    return db
+#Inserts user if user already does not exist in the db
+def insert_user(db_conn, username, password):
+    
+    if(check_user(db_conn, username)):
+        print("User already exists")
+    else:
+        user_auths = db_conn.user_auths
+
+        user_auth = {"u_name": username, "pwd": password}
+
+        user_auths.insert_one(user_auth).inserted_id
+        pprint.pprint(user_auths.find_one())
+    return 
+
+#Removes user if they are in the db.
+def delete_user(db_conn, username):
+    if check_user(db_conn, username) == False:
+        print("User does not exist")
+        return
+    
+    user_auths = db_conn.user_auths
+
+    user_auth = {"u_name": username}
+
+    user_auths.delete_one(user_auth)
+
+    return
+
+#Checks if there is a user by this username
+def check_user(db_conn, username):
+    if(db_conn.user_auths.find_one({"u_name": username})):
+        return True
+    
+    return False
+
+#Replaces the user with a new user
+def replace_user(db_conn, username):
+    if not check_user(db_conn, username):
+        return "User Exists"
+    else: 
+        delete_user(db_conn, username)
--- stonk-ponk-be/main.py
+++ stonk-ponk-be/main.py
@@ -1,55 +1,14 @@
 import pymongo
 from pymongo import MongoClient
 import pprint
+import database_func as db
 
 def main():
-    db = start_db()
-    insert_user(db, "Sampath", "Somanchi")
-    delete_user(db, "Sampath", "Somanchi")
-    print(check_user(db, "Sampath", "Somanchi"))
-    
-#Creates a connection to a mongodb created on localhost and returns db pointer/client
-def start_db():
-    client = MongoClient('localhost', 27017)
+    db_conn = db.start_db()
+    db.insert_user(db_conn, "Sampath", "Somanchi")
+    db.delete_user(db_conn, "Sampath")
+    print(db.check_user(db_conn, "Sampath"))
 
-    db = client.test_database
-
-    return db
-
-#Inserts user if user already does not exist in the db
-def insert_user(db, username, password):
-    
-    if(check_user(db, username, password)):
-        print("User already exists")
-    else:
-        user_auths = db.user_auths
-
-        user_auth = {"u_name": username, "pwd": password}
-
-        user_auths.insert_one(user_auth).inserted_id
-        pprint.pprint(user_auths.find_one())
-    return 
-
-#Removes user if they are in the db.
-def delete_user(db, username, password):
-    if check_user(db, username, password) == False:
-        print("User does not exist")
-        return
-    
-    user_auths = db.user_auths
-
-    user_auth = {"u_name": username, "pwd": password}
-
-    user_auths.delete_one(user_auth)
-
-    return
-
-#Checks if there is a user by this username
-def check_user(db, username, password):
-    if(db.user_auths.find_one({"u_name": username, "pwd": password})):
-        return True
-    
-    return False
 
 if __name__ == '__main__':
     main()
