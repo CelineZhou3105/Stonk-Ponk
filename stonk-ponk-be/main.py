1669a952a48bb7235d1a77ce4d882a73a8374432
Sampath Somanchi <sampathanator@gmail.com>
2021-03-13 19:23:34 +1100

    Sampath

--- stonk-ponk-be/main.py
+++ stonk-ponk-be/main.py
@@ -3,16 +3,18 @@ from pymongo import MongoClient
 import pprint
 import database_func as db
 
-#start up functions will be called to set up all connections. After this we will start a listener to take commands from the frontend
+#start up functions will be called to set up all connections. 
+# After this we will start a listener to take commands from the frontend
 def main():
     db_conn = db.start_db()
 
-    #set up a listener
+    #set up a server listener
     listen(db_conn)
     db.insert_user(db_conn, "Sampath", "Somanchi")
     db.delete_user(db_conn, "Sampath")
     print(db.check_user(db_conn, "Sampath"))
 
+#Server listener 
 def listen(db_conn):
     online = True
     while online: 
@@ -22,6 +24,10 @@ def listen(db_conn):
             online = False
             continue
 
+def get_func(command):
+    return{
+        'Quit': 
+    }
 
 if __name__ == '__main__':
     main()
