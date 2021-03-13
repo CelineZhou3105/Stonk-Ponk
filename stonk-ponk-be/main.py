6ef8aca90c7e62580d34866f9947f1061f31906b
Sampath Somanchi <sampathanator@gmail.com>
2021-03-13 20:58:46 +1100

    sever.py created

--- stonk-ponk-be/main.py
+++ stonk-ponk-be/main.py
@@ -2,38 +2,17 @@ import pymongo
 from pymongo import MongoClient
 import pprint
 import database_func as db
+import server
 
 #start up functions will be called to set up all connections. 
 # After this we will start a listener to take commands from the frontend
 def main():
     db_conn = db.start_db()
 
-    #set up a server listener
-    listen(db_conn)
     db.insert_user(db_conn, "Sampath", "Somanchi")
     db.delete_user(db_conn, "Sampath")
     print(db.check_user(db_conn, "Sampath"))
 
-#Server listener 
-def listen(db_conn):
-    online = True
-    while online: 
-        command_dict = {"command": "insert", "username": "sampath", "password": "pingas"}
-        
-        action = get_func(db_conn, command_dict)
-
-        if action == 'Quit':
-            online = False
-            continue
-
-        if action == "Unknown": 
-            print("We got an unknown command\n")
-
-def get_func(db_conn, command_string):
-    command = command_string["command"]
-
-    if command == "insert":
-        
 
 if __name__ == '__main__':
     main()
