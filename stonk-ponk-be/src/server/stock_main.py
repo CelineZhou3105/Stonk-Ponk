39230c4297ca77276afc018f4aeaa3560019c987
root <root@DESKTOP-J5PNC7T.localdomain>
2021-03-22 20:50:42 +1100

    QUUACK 32 All key stock data has been pulled based on ticker, needs to be transformed to feed specific front end requests

--- stonk-ponk-be/src/server/stock_main.py
+++ stonk-ponk-be/src/server/stock_main.py
@@ -2,7 +2,8 @@ from stocks import stock_api as si
 
 def main():
     print(si.get_market_status())
-    print(si.get_most_active())
+    print(si.get_stats('CBA.AX'))
+    print(si.get_quotes('CBA.AX'))
 
 if __name__ == '__main__':
     main()
--- stonk-ponk-be/src/server/stocks/stock_api.py
+++ stonk-ponk-be/src/server/stocks/stock_api.py
@@ -14,7 +14,7 @@ def get_top_ten():
     #top 10
     gainers = gainers.head(10)
     
-    return gainers.to_json(orient ='table')
+    return gainers.to_json()
 
 def get_bot_ten():
     losers = si.get_day_losers()
@@ -22,11 +22,29 @@ def get_bot_ten():
     #bottom 10
     losers = losers.head(10)
     
-    return losers.to_json(orient ='table')
+    return losers.to_json()
 
 def get_most_active():
     actives = si.get_day_most_active()
 
     actives = actives.head(10)
     
-    return actives.to_json(orient ='table')
+    return actives.to_json()
+
+def get_price(ticker):
+    try:
+        return si.get_live_price(ticker)
+    except: 
+        return "Stock Not Found"
+
+def get_stats(ticker):
+    try:
+        return si.get_stats(ticker)
+    except:
+        return "Stock Not Found"
+
+def get_quotes(ticker):
+    try:
+        return si.get_quote_table(ticker)
+    except:
+        return "Stock Not Found"
