9c8eee49994ec370748d1749779a20437ad79770
root <root@DESKTOP-J5PNC7T.localdomain>
2021-03-22 21:52:20 +1100

    QUUACK32: Front end can request for stock price and markets data from the backend

---
+++ stonk-ponk-be/src/server/stocks/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
+
+from . import views
+
+urlpatterns = [
+    path('markets', views.markets, name='markets')
+]
