5d01e057eb580362be815a394ee2898826242090
root <root@DESKTOP-J5PNC7T.localdomain>
2021-03-28 10:28:14 +1100

    urls added in api

--- stonk-ponk-be/src/server/api/urls.py
+++ stonk-ponk-be/src/server/api/urls.py
@@ -2,4 +2,11 @@ from django.urls import include, path
 
 urlpatterns = [
     path('account/', include('account.urls')),
+    
 ]
+
+urlpatterns = [
+    path('stocks/', include('stocks.urls')),
+    
+]
+
