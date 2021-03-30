5a2ad37ed87be7d6e119a0912d244b023b3b343f
ChaoQlate <z5122525@student.unsw.edu.au>
2021-03-30 21:51:13 +1100

    portfolio db draft done

--- stonk-ponk-be/src/server/portfolio/models.py
+++ stonk-ponk-be/src/server/portfolio/models.py
@@ -3,50 +3,96 @@ import uuid
 
 # Create your models here.
 
+def getPrice(trans):
+    return trans.purchase_price
+
+
 class Portfolio(models.Model) :
     email = models.EmailField(('email address'), unique=True)
     stocks = models.ManyToManyField("PortfolioStock", through = 'StockOwnership')
     
-    def add_stock(self, ticker_id, entry_time, amount_bought) :
+    def add_stock(self, ticker_id, date, volume, price) :
         # get this from market price
-        entry_price = 10
+        ownership, created = StockOwnership.objects.get_or_create(owner=self, stock=ticker_id)
+        ownership.VWAP = (ownership.VWAP * ownership.volume + price * volume) / (ownership.volume + volume)
+        ownership.volume = ownership.volume + volume 
+        ownership.save()
 
-        ownership = StockOwnership.objects.create(owner = self, stock = ticker_id, VWAP = entry_price, volume = amount_bought)
-        transaction = Transaction.objects.create(purchase_date = entry_time, stockOwnership = ownership)
+        transaction = Transaction.objects.create( 
+                stockOwnership = ownership,
+                purchase_date  = date, 
+                purchase_vol   = volume,
+                purchase_price = price)
 
    
-   # def remove_whole_transaction(uuid):
-   #     UUID_to_remove = uuid.UUID(uuid); 
-   #     transaction = Transactions.objects.get(id = UUID_to_remove)
-   #     #get stockownership from transaction
-   #     transaction.delete()
-   #     #search transactions matching on stockownership
-   #     #if empty, delete stock ownership
+    def remove_transaction(self, _uuid):
+        trans_id = uuid.UUID(_uuid); 
+        trans = Transaction.objects.get(id=trans_id)
+        trans_price = trans.purchase_price
+        trans_vol = trans.purchase_vol
 
-   # def remove_partial_transaction(uuid):
+        # update ownership
+        ownership = trans.stockOwnership
+        ownership.VWAP = (ownership.VWAP * ownership.volume - trans_price * trans_vol) / (ownership.volume - trans_volume)
+        ownership.volume = ownership.volume - trans_vol
+        ownership.save()
 
-   # def remove_all_transactions(ticker):
+        # remove ownership if last transaction
+        if ownership.volume == 0:
+            ownership.delete()
+        else:
+            trans.delete()
 
-   # def get_all_transactions_for_stock(ticker):
+    def update_transaction(self, _uuid, new_date, new_volume, new_price):
+        if new_volume <= 0:
+            return
 
-class PortfolioStock(models.Model) :
+        trans_id = uuid.UUID(_uuid); 
+        trans = Transaction.objects.get(id=trans_id)
+        old_price = trans.purchase_price
+        old_volume = trans.purchase_vol
+        vol_diff = new_volume - old_volume
+
+        # update ownership
+        ownership = trans.stockOwnership
+        ownership.VWAP = (ownership.VWAP * ownership.volume - old_price * old_volume + new_price * new_volume) / (ownership.volume + vol_diff)
+        ownership.volume = ownership.volume + vol_diff
+        ownership.save()
+
+        # update transaction
+        trans.purchase_vol   = new_volume
+        trans.purchase_price = new_price
+        trans.purchase_date  = new_date
+        trans.save()
+
+
+    def remove_ownership(self, ticker):
+        stock = PortfolioStock.objects.get(ticker=ticker)
+        ownership = StockOwnership.objects.get(owner=self,stock=stock)
+        ownership.delete()
+
+    def get_all_transactions_for_stock(self, ticker):
+        stock = PortfolioStock.objects.get(ticker=ticker)
+        stockOwnership = StockOwnership.objects.get(owner=self,stock=stock)
+        return Transaction.objects.filter(stockOwnership=stockOwnership)
+    
+    def get_stock_ownerships(self):
+        return StockOwnership.objects.get(owner=self)
+
+class PortfolioStock(models.Model):
     ticker = models.CharField(max_length = 20)
-
 
 class StockOwnership(models.Model): 
 
     owner = models.ForeignKey("Portfolio", on_delete = models.CASCADE)
     stock = models.ForeignKey("PortfolioStock", on_delete = models.CASCADE)
 
-    VWAP = models.FloatField()
-    volume = models.IntegerField()
+    VWAP = models.FloatField(default=0)
+    volume = models.IntegerField(default=0)
     
-  #  def get_all_transactions():
-
-
 class Transaction(models.Model):
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     purchase_date = models.DateField()
+    purchase_vol = models.IntegerField(default=0)
+    purchase_price = models.FloatField(default=0)
     stockOwnership = models.ForeignKey("StockOwnership", on_delete = models.CASCADE)
-
