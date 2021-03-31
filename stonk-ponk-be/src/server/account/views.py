276039497dc97bbc680ca8f5e135df242d4779e3
Ashwin Sarkar <ashwin.sarkar@hotmail.com>
2021-03-31 21:37:23 +1100

    added portfolio creation in registration

--- stonk-ponk-be/src/server/account/views.py
+++ stonk-ponk-be/src/server/account/views.py
@@ -9,6 +9,7 @@ from rest_framework_jwt.utils import jwt_decode_handler
 import jwt.exceptions 
 
 from .models import User
+from portfolio.models import *
 
 @require_http_methods(["POST"])
 def index(request):
@@ -46,7 +47,7 @@ def register(request):
                 , last_name=body["lastName"]
                 , security_question=body["securityQuestion"]
                 , security_answer=body["securityAnswer"])
-
+        portfolio = Portfolio.objects.create(email=body["email"])
         if user == None:
             return HttpResponseConflict()
     except:
