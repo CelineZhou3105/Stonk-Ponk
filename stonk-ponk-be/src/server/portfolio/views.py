783cc7b7f8d8feb3ea9089d930b44576c41948ea
ChaoQlate <z5122525@student.unsw.edu.au>
2021-04-01 13:20:32 +1100

    Portfolio details be

--- stonk-ponk-be/src/server/portfolio/views.py
+++ stonk-ponk-be/src/server/portfolio/views.py
@@ -3,18 +3,15 @@ import json
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.http import require_http_methods
 
-from rest_framework_jwt.views import verify_jwt_token
 from rest_framework_jwt.utils import jwt_decode_handler
 
 import jwt.exceptions 
 
+from account.models import User
 from .models import Portfolio, PortfolioStock, StockOwnership, Transaction
 
 '''
 request
-    start : int
-    end : int  [start, end)
-    ordering : enum -> alphabetical, hottest lets go with alphabetical ordering for now
     empty token is in http header
 
 response
@@ -23,7 +20,7 @@ response
             ticker: <string>
             volume: <integer>
             vwap  : <float>
-            first-purchase-date : <date>
+            first_purchase_date : <date>
             transactions : [
                 uuid  : <string>
                 volume: <integer>
@@ -32,20 +29,50 @@ response
             ]
         } ...
     ]
-
-
 '''
 
 @require_http_methods(["GET"])
 def details(request):
-    # get the token
+    try:
+        token = request.headers["Authorization"]
+        payload = jwt_decode_handler(token)
+        user = User.objects.get(id=payload["user_id"])
+        portfolio = Portfolio.objects.get(email=user.email)
+        holdings = StockOwnership.objects.filter(owner=portfolio)
 
+        ret = {"stocks": []}
 
-    # verify/match the token
-    # get the portfolio object
-    # get the stocks in the portfolio
-    #     get the stocks in the index x to x+1
-    pass
+        for h in holdings:
+            if h == None:
+                print(1)
+            print(h)
+            # TODO we should use query set sort or make use of meta
+            transactions = list(Transaction.objects.filter(stockOwnership=h))
+            transactions.sort(key=lambda x : x.purchase_date)
+            
+            retTrans = [
+                    {"uuid" : str(t.id),
+                     "volume": str(t.purchase_vol),
+                     "price": str(t.purchase_price),
+                     "date": str(t.purchase_date)} 
+                    for t in transactions]
+            print(h)
+            h.recalculate()
+
+            ret["stocks"].append( 
+                {
+                    "name": str(h.getStockName()),
+                    "ticker": str(h.getStockTicker()),
+                    "volume": str(h.volume),
+                    "vwap": str(h.VWAP),
+                    "first_purchase_date": str(h.first_purchase),
+                    "transactions": retTrans
+                })
+        return HttpResponse(json.dumps(ret))  
+    except Exception as e:
+        print(e.message)
+        return HttpResponseBadRequest("portfolio details bad request")
+    return HttpResponseBadRequest("portfolio details really bad request")
 
 '''
 request
@@ -94,10 +121,8 @@ request
             } ...]
         } ...]
     }
-
 response
     code from http
-
 '''
 @require_http_methods(["POST"])
 def edit(request):
