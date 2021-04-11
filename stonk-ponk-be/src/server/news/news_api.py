e01e9af073ef36f966e65d4fc0621ea69bbdf866
root <root@DESKTOP-J5PNC7T.localdomain>
2021-04-11 21:06:31 +1000

    QUUACK63 news api datetime removed

--- stonk-ponk-be/src/server/news/news_api.py
+++ stonk-ponk-be/src/server/news/news_api.py
@@ -2,6 +2,7 @@ from GoogleNews import GoogleNews
 from stocks import stock_api
 import pandas as pd
 import json
+import requests
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 #gets news for the last 1 week
@@ -9,12 +10,15 @@ def get_news(ticker):
     try:
         stock_data = stock_api.get_stock_data(ticker)
     except:
-        raise Error("Stock Not Found")
+        raise Exception("Stock Not Found")
     
     try:
         googlenews = GoogleNews(period='7d')
         googlenews.search(stock_data['name'])
         result = json.dumps(googlenews.result())
+        for item in result:
+            item.pop('datetime')
+            
         return result
     except:
         raise Error("News Error")
