091a300d9bd97a4b6f5fd80fae1bfbc8a041f3cb
Celine Zhou <z5207806@ad.unsw.edu.au>
2021-04-16 20:41:29 +1000

    added watchlist api call functions

---
+++ stonk-ponk-fe/src/services/watchlist.js
@@ -0,0 +1,132 @@
+import { GetWatchlistNameLink, CreateWatchlistLink, DeleteWatchlistLink, GetWatchlistStockLink, AddStockToWatchlistLink, RemoveStockFromWatchlistLink } from '../api-links/constants';
+
+const getWatchlistName = async () => {
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `${localStorage.getItem('token')}`,
+        },
+    };
+    return await fetch(GetWatchlistNameLink, requestOptions)
+        .then(response => {
+            if (response.ok) { // if status code is 200
+                return Promise.resolve(response);
+            } // if status code is not 200
+            return Promise.reject(response);
+        })
+}
+
+const getWatchlistStocks = async () => {
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `${localStorage.getItem('token')}`,
+        },
+    };
+    return await fetch(GetWatchlistStockLink, requestOptions)
+        .then(response => {
+            if (response.ok) { // if status code is 200
+                return Promise.resolve(response);
+            } // if status code is not 200
+            return Promise.reject(response);
+        })
+}
+
+const createWatchlist = async (watchlistName) => {
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `${localStorage.getItem('token')}`,
+        },
+        body: JSON.stringify({
+            name: watchlistName,
+        }),
+    };
+    return await fetch(CreateWatchlistLink, requestOptions)
+        .then(response => {
+            if (response.ok) { // if status code is 200
+                return response;
+            } // if status code is not 200
+            return Promise.reject(response);
+        })
+}
+
+const deleteWatchlist = async (id) => {
+    const requestOptions = {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `${localStorage.getItem('token')}`,
+        },
+        body: JSON.stringify({
+            watchlist_id: id,
+        }),
+    };
+    return await fetch(DeleteWatchlistLink, requestOptions)
+        .then(response => {
+            if (response.ok) { // if status code is 200
+                return response;
+            } // if status code is not 200
+            return Promise.reject(response);
+        })
+}
+
+const addStockToWatchlist = async (id, ticker) => {
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `${localStorage.getItem('token')}`,
+        },
+        body: JSON.stringify({
+            watchlist_id: id,
+            ticker: ticker,
+        }),
+    };
+    return await fetch(AddStockToWatchlistLink, requestOptions)
+        .then(response => {
+            if (response.ok) { // if status code is 200
+                return response;
+            } // if status code is not 200
+            return Promise.reject(response);
+        })
+}
+
+const removeStockFromWatchlist = async (id, ticker) => {
+    const requestOptions = {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': `${localStorage.getItem('token')}`,
+        },
+        body: JSON.stringify({
+            watchlist_id: id,
+            ticker: ticker,
+        }),
+    };
+    return await fetch(RemoveStockFromWatchlistLink, requestOptions)
+        .then(response => {
+            if (response.ok) { // if status code is 200
+                return response;
+            } // if status code is not 200
+            return Promise.reject(response);
+        })
+}
+
+export const watchlist = {
+    getWatchlistName,
+    getWatchlistStocks,
+    createWatchlist,
+    deleteWatchlist,
+    addStockToWatchlist,
+    removeStockFromWatchlist,
+};
