9877907117ed953ebff4c31feedc7928aaf8a46c
ChaoQlate <z5122525@student.unsw.edu.au>
2021-04-19 22:35:33 +1000

    mini merge master

--- stonk-ponk-be/src/server/watchlist/views.py
+++ stonk-ponk-be/src/server/watchlist/views.py
@@ -5,36 +5,30 @@ import json
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.http import require_http_methods
 
-from rest_framework_jwt.utils import jwt_decode_handler
-
-import jwt.exceptions 
-
 from account.models import User
+from account.auth import require_token, get_user
 from portfolio.models import Portfolio, PortfolioStock, StockOwnership, Transaction
 from stocks import stock_api
 from watchlist.models import Watchlist, StockWatch
 
 @require_http_methods(["POST"])
+@require_token
 def create_watchlist(request):
-    token = request.headers["Authorization"]
-    payload = jwt_decode_handler(token)
-    user = User.objects.get(id=payload["user_id"])
-
     body = json.loads(request.body.decode("utf-8"))
-    r_email = body["email"]
-    watchlist_name = body["watchlist_name"] 
+    user = get_user(request)
+    name = body["watchlist_name"] 
 
-    Watchlist.objects.create(user = user, name = watchlist_name) 
-
-    return HttpResponse()
+    # how do we want to handle things that have been aleady created?
+    wl = Watchlist.objects.create(user = user, name = name) 
+    responseData = { "watchlist_id": wl.id }
+    return HttpResponse(json.dumps(responseData))
 
 @require_http_methods(["DELETE"])
+@require_token
 def delete_watchlist(request):
-    token = request.headers["Authorization"]
-    payload = jwt_decode_handler(token)
-    user = User.objects.get(id=payload["user_id"])
-
     body = json.loads(request.body.decode("utf-8"))
+    user = get_user(request)
+
     watchlist_id = body["watchlist_id"]
 
     try:
@@ -45,59 +39,38 @@ def delete_watchlist(request):
     except Watchlist.DoesNotExist:
         return HttpResponseNotFound()
     
-    return HttpResponse()    
+    return HttpResponse() 
 
 @require_http_methods(["POST"])
-def add_stock_to_watchlist(request):
-    token = request.headers["Authorization"]
-    payload = jwt_decode_handler(token)
-    user = User.objects.get(id=payload["user_id"])
-    
+@require_token
+def save_watchlist(request):
     body = json.loads(request.body.decode("utf-8"))
-    ticker = body["ticker"]
+    user = get_user(request)
+
     watchlist = body["watchlist_id"]
     
     try:
         wl = Watchlist.objects.get(id = watchlist, user = user)
-        wl.add_stock(ticker)
+        wl.save_stocks(set(body["tickers"]))
     except Watchlist.DoesNotExist:
         return HttpResponseNotFound() 
     
-    return HttpResponse()    
-
-@require_http_methods(["DELETE"])
-def remove_stock_from_watchlist(request):
-    token = request.headers["Authorization"]
-    payload = jwt_decode_handler(token)
-    user = User.objects.get(id=payload["user_id"])
-    
-    body = json.loads(request.body.decode("utf-8"))
-    ticker = body["ticker"]
-    watchlist = body["watchlist_id"]
-
-    try:
-        wl = Watchlist.objects.get(id = watchlist, user = user)
-        wl.del_stock(ticker)
-    except Watchlist.DoesNotExist:
-        return HttpResponseNotFound() 
-
     return HttpResponse()    
 
 @require_http_methods(["GET"])
+@require_token
 def get_watchlists(request):
     #get all watchlists associated with a user 
-   
-    token = request.headers["Authorization"]
-    payload = jwt_decode_handler(token)
-    user = User.objects.get(id=payload["user_id"])
-
+    user = get_user(request)
     ret = {"watchlists" : []}
+
     for wl in Watchlist.objects.filter(user = user):
         ret["watchlists"].append({"id" : wl.id, "label" : wl.name})
 
     return HttpResponse(json.dumps(ret))
 
 @require_http_methods(["GET"])
+@require_token
 def get_watchlist_stocks(request):
     #given a watchlist id, return json array of stocks with relevant information
     wid = int(request.GET.get("id"))
