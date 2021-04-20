83321e3db03d178e5dbcac4031221c0d3eefb3f5
root <root@DESKTOP-J5PNC7T.localdomain>
2021-04-20 14:47:20 +1000

    forex tested and working

--- stonk-ponk-be/src/server/api_interface/alphavantage_forex_api.py
+++ stonk-ponk-be/src/server/api_interface/alphavantage_forex_api.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 class AaForexApi():
     def __init__(self):
@@ -7,8 +8,16 @@ class AaForexApi():
     def get_currency_exchange(self, from_currency, to_currency):
         try:
             request_string = str("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=" + from_currency + "&to_currency=" + to_currency + "&apikey=RYJOHTXNG0NQFQVQ")
-            exchange_rate = requests.get(request_string)
+            response = requests.get(request_string)
             self.num_calls += 1
-            return exchange_rate
+            
+            if response.status_code == 200:
+                values = json.loads(response.text)
+                exchange_rate = values['Realtime Currency Exchange Rate']['5. Exchange Rate']
+                    
+                return exchange_rate
+
+            else:
+                return response.json()
         except:
             return "Incorrect Currencies"
--- stonk-ponk-be/src/server/stocks/tests.py
+++ stonk-ponk-be/src/server/stocks/tests.py
@@ -6,9 +6,12 @@ from api_interface.forex_api_interface import ForexApiInterface as f_api
 class ForexTestCase(TestCase):
     def setUp(self):
         self.f_api = f_api
+        self.from_currency = "USD"
+        self.to_currency = "AUD"
     
     def test_forex_aud_usd(self):
-        from_currency = "USD"
-        to_currency = "AUD"
-        print(self.f_api.get_currency_exchange(from_currency, to_currency))
-        
+        print(self.f_api.get_currency_exchange(self.from_currency, self.to_currency))
+    
+    def test_100_aud_usd(self):
+        print(self.f_api.calc_forex_rate(100, self.from_currency, self.to_currency))
+    
