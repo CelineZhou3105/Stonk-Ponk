3469ef2e1e1377590191ddb0c57a07e5383321ad
CelineZhou3105 <71369583+CelineZhou3105@users.noreply.github.com>
2021-05-04 21:16:49 +1000

    transferring files from private repo

---
+++ account/auth.py
@@ -0,0 +1,31 @@
+import json
+
+from django.http import HttpResponseForbidden
+
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.utils import jwt_decode_handler
+
+from jwt.exceptions import PyJWTError
+
+from .models import User
+
+'''
+called on a request route so we are wrapping a function with signature of function(request)
+'''
+def require_token(func):
+    def inner(*args, **kwargs):
+        try:
+            request = args[0]
+            user = get_user(request)
+        except PyJWTError:
+            return HttpResponseForbidden()
+        except:
+            return HttpResponseForbidden()
+        return func(*args, **kwargs)
+    return inner
+
+def get_user(request):
+    token = request.headers["Authorization"]
+    payload = jwt_decode_handler(token)
+    user = User.objects.get(id=payload["user_id"])
+    return user
